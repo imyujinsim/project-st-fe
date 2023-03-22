140613daa8d12c22b8c35f792f612ee894bccad5
yujin <imyujinsim@gmail.com>
2023-03-22 18:20:33 +0900

    20230322

--- src/component/ListItem.jsx
+++ src/component/ListItem.jsx
@@ -1,23 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../style/reset.css';
 import '../style/card.css';
-
-// const icon = document.getElementById('one');
-
-// function checkBookmark() {
-// icon.classList.toggle('checked');
-// if (icon.classList.contains('checked')) {
-//   // checked라는 클래스가 있으면 할 일
-//   icon.classList.toggle('checked');
-//   // 데이터베이스에서 북마크 저장한거 삭제
-// } else {
-//   // checked라는 클래스가 없으면 할 일
-//   icon.classList.add('checked');
-//   // 데이터베이스에 북마크 저장했다고 넣기
-// }
-// }
-
-// icon.addEventListener('click', checkBookmark);
 
 function ListItem(props) {
   const index = props.index;
