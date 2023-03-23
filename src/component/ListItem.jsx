cb43ff67d11d42cc4d5b1e2fc5638d93e2597c6c
yujin <imyujinsim@gmail.com>
2023-03-23 15:44:00 +0900

    axios

--- src/component/ListItem.jsx
+++ src/component/ListItem.jsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useState } from "react";
-import "../style/reset.css";
-import "../style/card.css";
+import React, { useCallback, useState } from 'react';
+import '../style/reset.css';
+import '../style/card.css';
 
 function ListItem(props) {
   const index = props.index;
--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Footer from '../layout/Footer';
 import Header from '../layout/Header';
 import Search from '../component/Search';
 import Result from '../searchResult/Result';
+import axios from 'axios';
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 
-function SearchResult(props) {
+export default function SearchResult() {
+  const key = encodeURIComponent(process.env.REACT_APP_TOUR);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    // axios 안돼요ㅠㅠㅠㅠㅠ
+    const getData = async () => {
+      await axios
+        .get('/api', {
+          baseURL: '/searchKeyword1',
+          params: {
+            numOfRows: 10,
+            pageNo: 1,
+            MobileOS: 'ETC',
+            MobileApp: 'STTour',
+            _type: 'json',
+            listYN: 'Y',
+            arrange: 'A',
+            keyword: 'busan',
+            contentTypeId: 12,
+            serviceKey: key,
+          },
+        })
+        .then((res) => setData(res));
+    };
+    void getData();
+  }, []);
+
   return (
     <>
       <Header />
       <hr />
       <Search />
-      <Result />
+      <Result data={data} />
       <Footer />
     </>
   );
 }
 
-export default SearchResult;
+// async function getData() {
+//   try {
+//     await axios
+//       .get('/searchKeyword1', {
+//         baseURL: 'https://apis.data.go.kr/B551011/KorService1',
+//         params: {
+//           numOfRows: '10',
+//           pageNo: '1',
+//           MobileOS: 'ETC',
+//           MobileApp: 'STTour',
+//           _type: 'json',
+//           listYN: 'Y',
+//           arrange: 'A',
+//           keyword: 'busan',
+//           contentTypeId: '12',
+//           serviceKey: key,
+//         },
+//       })
+//       .then((result) => console.log(result.data))
+//       .catch(console.log);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
