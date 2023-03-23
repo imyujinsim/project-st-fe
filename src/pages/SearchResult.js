f3b58649da2a4eeb938382f5f5ef01a391ec6d0e
user <main134679@gmail.com>
2023-03-23 15:38:51 +0900

    hello

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Footer from '../layout/Footer';
-import Header from '../layout/Header';
-import Search from '../component/Search';
-import Result from '../searchResult/Result';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+import Search from "../component/Search";
+import Result from "../searchResult/Result";
+import axios from "axios";
+
+// const env = require('dotenv').config();
+
+async function getData() {
+  try {
+    const client = axios.create({
+      baseURL: "http://apis.data.go.kr/B551011/KorService1",
+      headers: {
+        ServiceKey: process.env.REACT_APP_TOUR,
+      },
+    });
+
+    const response = await client.get("/searchKeyword", {
+      params: {
+        numOfRows: 10,
+        pageNo: 1,
+        MobileOS: "ETC",
+        MobileApp: "STTour",
+        _type: "json",
+        listYN: "Y",
+        arrange: "A",
+        keyword: "강원",
+        contentTypeId: 12,
+      },
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function SearchResult(props) {
+  // console.log(env);
   return (
     <>
       <Header />
       <hr />
       <Search />
-      <Result />
+      <Result data={getData()} />
       <Footer />
     </>
   );
---
+++ src/service/api.js
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// const URL = "/B551011/KorService1";
+
+// const response = axios.get(URL, {
+//   params: {
+//     serviceKey: process.env.REACT_APP_TOUR,
+//     numOfRows: 1,
+//     pasgeNo: 10,
+//   },
+// });
