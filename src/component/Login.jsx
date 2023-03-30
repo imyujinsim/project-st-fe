d8607b43357bbc8dd2f68b11bd00ce5000f70e74
yujin <imyujinsim@gmail.com>
2023-03-30 10:24:26 +0900

    test axios

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,33 +1,46 @@
-import React from "react";
-import logo from "../resources/logo.png";
-import "../style/reset.css";
-import "../style/login.css";
-import { Link } from "react-router-dom";
-import Cookies from "js-cookie";
-import { useLocation, useNavigate } from "react-router-dom";
+import React from 'react';
+import logo from '../resources/logo.png';
+import '../style/reset.css';
+import '../style/login.css';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Login(props) {
-  const LoginPage = () => {
-    const navigate = useNavigate();
-    const { state } = useLocation();
+  // const LoginPage = () => {
+  //   const navigate = useNavigate();
+  //   const { state } = useLocation();
 
-    const handleLogin = () => {
-      Cookies.set("id", "id");
+  //   const handleLogin = () => {
+  //     Cookies.set("id", "id");
 
-      if (state) {
-        navigate(state);
-      } else {
-        navigate("/login");
-      }
-    };
+  //     if (state) {
+  //       navigate(state);
+  //     } else {
+  //       navigate("/login");
+  //     }
+  //   };
+  // };
+  const url = 'http://3.36.124.165:8080/account/login';
+
+  const handleSubmit = async () => {
+    await axios
+      .post(url, { id: 'donghee1234', passwd: 'donghee1234' })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
+
   return (
     <div className="login-wrapper">
       <div className="login-form-wrapper">
         <h2>
           <img src={logo} width={300} />
         </h2>
-        <form method="post" action="" id="login-form">
+        <form method="post" action="" id="login-form" onSubmit={handleSubmit}>
           <input type="text" name="userName" placeholder="아이디(이메일)" />
           <input type="password" name="userPassword" placeholder="비밀번호" />
           <label htmlFor="remember-check">
@@ -37,10 +50,10 @@ function Login(props) {
           <input type="submit" defaultValue="로그인" />
           <Link to="/signup">
             <button
-              onClick={LoginPage.handleLogin}
+              // onClick={LoginPage.handleLogin}
               type="button"
               className="btn text"
-              style={{ float: "right" }}
+              style={{ float: 'right' }}
             >
               회원가입
             </button>
