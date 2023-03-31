6a2aa53dc17371c2c495f8280e8f421adb737c58
yujin <imyujinsim@gmail.com>
2023-03-31 17:52:07 +0900

    bookmark

--- src/component/ListItem.jsx
+++ src/component/ListItem.jsx
@@ -1,85 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../style/reset.css';
-import '../style/card.css';
-import axios from 'axios';
-
-function ListItem(props) {
-  const data = props.contentid;
-  useEffect(() => {
-    const useOnHandleClickLike = (e) => {
-      e.preventDefault();
-
-      const wishlist = [];
-      if (
-        wishlist.filter(
-          (InwishlistItem) => InwishlistItem.id === e.target.id
-        )[0]
-      ) {
-        e.target.classList.add('checked');
-      }
-
-      if (
-        !wishlist.filter(
-          (InwishlistItem) => InwishlistItem.id === e.target.id
-        )[0]
-      ) {
-        console.log('like clicked');
-        e.target.classList.add('checked');
-        // 디비에 넣는 코드
-      } else {
-        console.log('like canceled');
-        e.target.classList.remove('checked');
-        // 디비에서 빼는 코드
-      }
-      return;
-    };
-  }, []);
-
-  // function click(e) {
-  //   e.preventDefault();
-  //   if (e.target.classList.contains('checked')) {
-  //     e.target.classList.remove('checked');
-  //     // 데이터베이스에서 삭제
-  //   } else {
-  //     e.target.classList.add('checked');
-  //     // 데이터베이스에 추가
-
-  //     console.log(e.target.id);
-  //     console.log(props.picture);
-  //     console.log(props.name);
-  //   }
-  // }
-
-  return (
-    <Link
-      to={`/detail/${data}`}
-      onClick={() => {
-        window.localStorage.setItem('detail', props.name);
-        window.localStorage.setItem('contentId', data);
-      }}
-    >
-      <li className="list-item" data-target="card" id={data}>
-        <a href="">
-          <img src={props.picture} alt={props.name} />
-          <h3>{props.name}</h3>
-        </a>
-
-        <button
-          className="bookmark"
-          id={data}
-          onClick={(e) => {
-            useOnHandleClickLike(e);
-          }}
-        ></button>
-      </li>
-    </Link>
-  );
-}
-
-export default ListItem;
-
-//-------------
 // import React, { useEffect, useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import '../style/reset.css';
@@ -88,22 +6,50 @@ export default ListItem;
 
 // function ListItem(props) {
 //   const data = props.contentid;
-//   const [isBookmarked, setIsBookmarked] = useState(false);
-
 //   useEffect(() => {
-//     // localStorage에서 북마크 상태를 가져옵니다.
-//     const storedBookmark = window.localStorage.getItem(data);
-//     setIsBookmarked(storedBookmark === 'true');
-//   }, [data]);
+//     const useOnHandleClickLike = (e) => {
+//       e.preventDefault();
 
-//   function handleClick(e) {
-//     e.preventDefault();
-//     const newIsBookmarked = !isBookmarked;
-//     setIsBookmarked(newIsBookmarked);
-//     // localStorage에 북마크 상태를 저장합니다.
-//     window.localStorage.setItem(data, newIsBookmarked);
-//     // 데이터베이스에도 북마크 상태를 저장하면 됩니다.
-//   }
+//       const wishlist = [];
+//       if (
+//         wishlist.filter(
+//           (InwishlistItem) => InwishlistItem.id === e.target.id
+//         )[0]
+//       ) {
+//         e.target.classList.add('checked');
+//       }
+
+//       if (
+//         !wishlist.filter(
+//           (InwishlistItem) => InwishlistItem.id === e.target.id
+//         )[0]
+//       ) {
+//         console.log('like clicked');
+//         e.target.classList.add('checked');
+//         // 디비에 넣는 코드
+//       } else {
+//         console.log('like canceled');
+//         e.target.classList.remove('checked');
+//         // 디비에서 빼는 코드
+//       }
+//       return;
+//     };
+//   }, []);
+
+//   // function click(e) {
+//   //   e.preventDefault();
+//   //   if (e.target.classList.contains('checked')) {
+//   //     e.target.classList.remove('checked');
+//   //     // 데이터베이스에서 삭제
+//   //   } else {
+//   //     e.target.classList.add('checked');
+//   //     // 데이터베이스에 추가
+
+//   //     console.log(e.target.id);
+//   //     console.log(props.picture);
+//   //     console.log(props.name);
+//   //   }
+//   // }
 
 //   return (
 //     <Link
@@ -118,9 +64,13 @@ export default ListItem;
 //           <img src={props.picture} alt={props.name} />
 //           <h3>{props.name}</h3>
 //         </a>
+
 //         <button
-//           className={`bookmark ${isBookmarked ? 'checked' : ''}`}
-//           onClick={handleClick}
+//           className="bookmark"
+//           id={data}
+//           onClick={(e) => {
+//             useOnHandleClickLike(e);
+//           }}
 //         ></button>
 //       </li>
 //     </Link>
@@ -128,3 +78,53 @@ export default ListItem;
 // }
 
 // export default ListItem;
+
+//-------------
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../style/reset.css';
+import '../style/card.css';
+import axios from 'axios';
+
+function ListItem(props) {
+  const data = props.contentid;
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  useEffect(() => {
+    // localStorage에서 북마크 상태를 가져옵니다.
+    const storedBookmark = window.localStorage.getItem(data);
+    setIsBookmarked(storedBookmark === 'true');
+  }, [data]);
+
+  function handleClick(e) {
+    e.preventDefault();
+    const newIsBookmarked = !isBookmarked;
+    setIsBookmarked(newIsBookmarked);
+    // localStorage에 북마크 상태를 저장합니다.
+    window.localStorage.setItem(data, newIsBookmarked);
+    // 데이터베이스에도 북마크 상태를 저장하면 됩니다.
+  }
+
+  return (
+    <Link
+      to={`/detail/${data}`}
+      onClick={() => {
+        window.localStorage.setItem('detail', props.name);
+        window.localStorage.setItem('contentId', data);
+      }}
+    >
+      <li className="list-item" data-target="card" id={data}>
+        <a href="">
+          <img src={props.picture} alt={props.name} />
+          <h3>{props.name}</h3>
+        </a>
+        <button
+          className={`bookmark ${isBookmarked ? 'checked' : ''}`}
+          onClick={handleClick}
+        ></button>
+      </li>
+    </Link>
+  );
+}
+
+export default ListItem;
