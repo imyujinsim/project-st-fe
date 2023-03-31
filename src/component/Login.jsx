e4a1d3a007f0bf250da57dfdd4bd2ae4277dd9e1
yujin <imyujinsim@gmail.com>
2023-03-31 15:23:26 +0900

    mypage

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -45,7 +45,7 @@ function Login(props) {
         console.log(response);
         localStorage.setItem('token', response.data.token);
         console.log(localStorage.getItem('token'));
-        window.location.href = '/';
+        window.location.href = 'https://www.bodam.site';
       })
       .catch(function (error) {
         console.log(error);
