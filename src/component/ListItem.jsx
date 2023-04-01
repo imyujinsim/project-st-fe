70114ca0fd2553e331415a02bdf2578d4d27a518
yujin <imyujinsim@gmail.com>
2023-04-01 11:46:43 +0900

    bookmark

--- src/component/ListItem.jsx
+++ src/component/ListItem.jsx
@@ -1,83 +1,95 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import '../style/reset.css';
-// import '../style/card.css';
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../style/reset.css';
+import '../style/card.css';
+import axios from 'axios';
 
-// function ListItem(props) {
-//   const data = props.contentid;
-//   useEffect(() => {
-//     const useOnHandleClickLike = (e) => {
-//       e.preventDefault();
+function ListItem(props) {
+  const data = props.contentid;
 
-//       const wishlist = [];
-//       if (
-//         wishlist.filter(
-//           (InwishlistItem) => InwishlistItem.id === e.target.id
-//         )[0]
-//       ) {
-//         e.target.classList.add('checked');
-//       }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [likedItems, setLikedItems] = useState([]);
 
-//       if (
-//         !wishlist.filter(
-//           (InwishlistItem) => InwishlistItem.id === e.target.id
-//         )[0]
-//       ) {
-//         console.log('like clicked');
-//         e.target.classList.add('checked');
-//         // 디비에 넣는 코드
-//       } else {
-//         console.log('like canceled');
-//         e.target.classList.remove('checked');
-//         // 디비에서 빼는 코드
-//       }
-//       return;
-//     };
-//   }, []);
+  useEffect(() => {
+    // local storage 사용해서 페이지 로딩 시 북마크 되어있는 항목 표시
+    setIsLoggedIn = window.localStorage.getItem('token') ? true : false;
+    if(isLoggedIn) {
+      const storedLikedItems = JSON.parse(localStorage.getItem('likedItems'));
+      if(storedLikedItems) {
+        setLikedItems(storedLikedItems);
+      }
+    }
+  }, []);
 
-//   // function click(e) {
-//   //   e.preventDefault();
-//   //   if (e.target.classList.contains('checked')) {
-//   //     e.target.classList.remove('checked');
-//   //     // 데이터베이스에서 삭제
-//   //   } else {
-//   //     e.target.classList.add('checked');
-//   //     // 데이터베이스에 추가
+  useEffect(() => {
+    // likedItems 배열이 업데이트 될 때마다 localStorage에 저장
+    localStorage.setItem('likedItems', JSON.stringify(likedItems));
+  }, [likedItems]);
 
-//   //     console.log(e.target.id);
-//   //     console.log(props.picture);
-//   //     console.log(props.name);
-//   //   }
-//   // }
+  const useOnHandleClickLike = (e, contentId) => {
+    e.preventDefault();
 
-//   return (
-//     <Link
-//       to={`/detail/${data}`}
-//       onClick={() => {
-//         window.localStorage.setItem('detail', props.name);
-//         window.localStorage.setItem('contentId', data);
-//       }}
-//     >
-//       <li className="list-item" data-target="card" id={data}>
-//         <a href="">
-//           <img src={props.picture} alt={props.name} />
-//           <h3>{props.name}</h3>
-//         </a>
+    const index = likedItems.findIndex((item) => item === contentId);
 
-//         <button
-//           className="bookmark"
-//           id={data}
-//           onClick={(e) => {
-//             useOnHandleClickLike(e);
-//           }}
-//         ></button>
-//       </li>
-//     </Link>
-//   );
-// }
+    if (index > -1) {
+      // 이미 좋아요 상태인 경우, likedItems 배열에서 해당 contentId를 삭제합니다.
+      setLikedItems(likedItems.filter((item) => item !== contentId));
+      e.target.classList.remove('checked');
+      // 서버에서 해당 contentId를 좋아요 취소 처리합니다.
+      // await axios.delete(`/api/items/${contentId}/like`);
+    } else {
+      // 좋아요 상태가 아닌 경우, likedItems 배열에 해당 contentId를 추가합니다.
+      setLikedItems([...likedItems, contentId]);
+      e.target.classList.add('checked');
+      // 서버에서 해당 contentId를 좋아요 처리합니다.
+      // await axios.post(`/api/items/${contentId}/like`);
+    }
+  };
+    // function click(e) {
+    //   e.preventDefault();
+    //   if (e.target.classList.contains('checked')) {
+    //     e.target.classList.remove('checked');
+    //     // 데이터베이스에서 삭제
+    //   } else {
+    //     e.target.classList.add('checked');
+    //     // 데이터베이스에 추가
+      
+    //     console.log(e.target.id);
+    //     console.log(props.picture);
+    //     console.log(props.name);
+    //   }
+    // }
+    }
 
-// export default ListItem;
+  return (
+    <Link
+      to={`/detail/${data}`}
+      onClick={() => {
+        window.localStorage.setItem('detail', props.name);
+        window.localStorage.setItem('contentId', data);
+      }}
+    >
+      <li className="list-item" data-target="card" id={data}>
+        <a href="">
+          <img src={props.picture} alt={props.name} />
+          <h3>{props.name}</h3>
+        </a>
+
+        {isLoggedIn ? (
+          <button
+          className="bookmark"
+          id={data}
+          onClick={(e) => {
+            useOnHandleClickLike(e, data);
+          }}
+        ></button>
+        ) : ''}
+      </li>
+    </Link>
+  );
+}
+
+export default ListItem;
 
 //-------------
 import React, { useEffect, useState } from 'react';
