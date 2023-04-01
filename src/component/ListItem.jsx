93779045d93bd5b3904f9b21451d0076eef8e0dd
yujin <imyujinsim@gmail.com>
2023-04-01 11:49:26 +0900

    bookmark

--- src/component/ListItem.jsx
+++ src/component/ListItem.jsx
@@ -13,9 +13,9 @@ function ListItem(props) {
   useEffect(() => {
     // local storage 사용해서 페이지 로딩 시 북마크 되어있는 항목 표시
     setIsLoggedIn = window.localStorage.getItem('token') ? true : false;
-    if(isLoggedIn) {
+    if (isLoggedIn) {
       const storedLikedItems = JSON.parse(localStorage.getItem('likedItems'));
-      if(storedLikedItems) {
+      if (storedLikedItems) {
         setLikedItems(storedLikedItems);
       }
     }
@@ -45,21 +45,20 @@ function ListItem(props) {
       // await axios.post(`/api/items/${contentId}/like`);
     }
   };
-    // function click(e) {
-    //   e.preventDefault();
-    //   if (e.target.classList.contains('checked')) {
-    //     e.target.classList.remove('checked');
-    //     // 데이터베이스에서 삭제
-    //   } else {
-    //     e.target.classList.add('checked');
-    //     // 데이터베이스에 추가
-      
-    //     console.log(e.target.id);
-    //     console.log(props.picture);
-    //     console.log(props.name);
-    //   }
-    // }
-    }
+  // function click(e) {
+  //   e.preventDefault();
+  //   if (e.target.classList.contains('checked')) {
+  //     e.target.classList.remove('checked');
+  //     // 데이터베이스에서 삭제
+  //   } else {
+  //     e.target.classList.add('checked');
+  //     // 데이터베이스에 추가
+
+  //     console.log(e.target.id);
+  //     console.log(props.picture);
+  //     console.log(props.name);
+  //   }
+  // }
 
   return (
     <Link
@@ -77,13 +76,15 @@ function ListItem(props) {
 
         {isLoggedIn ? (
           <button
-          className="bookmark"
-          id={data}
-          onClick={(e) => {
-            useOnHandleClickLike(e, data);
-          }}
-        ></button>
-        ) : ''}
+            className="bookmark"
+            id={data}
+            onClick={(e) => {
+              useOnHandleClickLike(e, data);
+            }}
+          ></button>
+        ) : (
+          ''
+        )}
       </li>
     </Link>
   );
@@ -92,63 +93,63 @@ function ListItem(props) {
 export default ListItem;
 
 //-------------
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../style/reset.css';
-import '../style/card.css';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import '../style/reset.css';
+// import '../style/card.css';
+// import axios from 'axios';
 
-function BookmarkAlert() {
-  return <div>로그인이 필요합니다.</div>;
-}
+// function BookmarkAlert() {
+//   return <div>로그인이 필요합니다.</div>;
+// }
 
-function ListItem(props) {
-  const data = props.contentid;
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+// function ListItem(props) {
+//   const data = props.contentid;
+//   const [isBookmarked, setIsBookmarked] = useState(false);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    // localStorage에서 북마크 상태를 가져옵니다.
-    const storedBookmark = window.localStorage.getItem(data);
-    setIsBookmarked(storedBookmark === 'true');
-    // jwt 토큰이 있는지 확인합니다.
-    const jwtToken = window.localStorage.getItem('jwtToken');
-    setIsLoggedIn(!!jwtToken);
-  }, [data]);
+//   useEffect(() => {
+//     // localStorage에서 북마크 상태를 가져옵니다.
+//     const storedBookmark = window.localStorage.getItem(data);
+//     setIsBookmarked(storedBookmark === 'true');
+//     // jwt 토큰이 있는지 확인합니다.
+//     const jwtToken = window.localStorage.getItem('jwtToken');
+//     setIsLoggedIn(!!jwtToken);
+//   }, [data]);
 
-  function handleClick(e) {
-    e.preventDefault();
-    const newIsBookmarked = !isBookmarked;
-    setIsBookmarked(newIsBookmarked);
-    // localStorage에 북마크 상태를 저장합니다.
-    window.localStorage.setItem(data, newIsBookmarked);
-    // 데이터베이스에도 북마크 상태를 저장하면 됩니다.
-  }
+//   function handleClick(e) {
+//     e.preventDefault();
+//     const newIsBookmarked = !isBookmarked;
+//     setIsBookmarked(newIsBookmarked);
+//     // localStorage에 북마크 상태를 저장합니다.
+//     window.localStorage.setItem(data, newIsBookmarked);
+//     // 데이터베이스에도 북마크 상태를 저장하면 됩니다.
+//   }
 
-  return (
-    <Link
-      to={`/detail/${data}`}
-      onClick={() => {
-        window.localStorage.setItem('detail', props.name);
-        window.localStorage.setItem('contentId', data);
-      }}
-    >
-      <li className="list-item" data-target="card" id={data}>
-        <a href="">
-          <img src={props.picture} alt={props.name} />
-          <h3>{props.name}</h3>
-        </a>
-        {isLoggedIn ? (
-          <button
-            className={`bookmark ${isBookmarked ? 'checked' : ''}`}
-            onClick={handleClick}
-          ></button>
-        ) : (
-          <BookmarkAlert />
-        )}
-      </li>
-    </Link>
-  );
-}
+//   return (
+//     <Link
+//       to={`/detail/${data}`}
+//       onClick={() => {
+//         window.localStorage.setItem('detail', props.name);
+//         window.localStorage.setItem('contentId', data);
+//       }}
+//     >
+//       <li className="list-item" data-target="card" id={data}>
+//         <a href="">
+//           <img src={props.picture} alt={props.name} />
+//           <h3>{props.name}</h3>
+//         </a>
+//         {isLoggedIn ? (
+//           <button
+//             className={`bookmark ${isBookmarked ? 'checked' : ''}`}
+//             onClick={handleClick}
+//           ></button>
+//         ) : (
+//           <BookmarkAlert />
+//         )}
+//       </li>
+//     </Link>
+//   );
+// }
 
-export default ListItem;
+// export default ListItem;
