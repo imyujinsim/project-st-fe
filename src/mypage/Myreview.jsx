cc873659ab08c02164fab3a689e5b05ecc4d57ed
yujin <imyujinsim@gmail.com>
2023-04-03 20:43:42 +0900

    test review

--- src/mypage/Myreview.jsx
+++ src/mypage/Myreview.jsx
@@ -5,26 +5,58 @@ import TableItem from '../component/TableItem';
 import Modal from '../component/Modal';
 
 function Myreview(props) {
+  const token = localStorage.getItem('token');
+
+  const [reviewData, setReviewData] = useState([]);
+  const [reviewList, setReviewList] = useState([]);
+
+  useEffect(() => {
+    const getReviewData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.bodam.site:8080/board/mylist/`,
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response);
+        setReviewData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getReviewData();
+  }, [count]);
+
+  useEffect(() => {
+    const reviews = reviewData
+      .filter((item) => item !== undefined && item !== null)
+      .map((item) => {
+        return (
+          <TableItem
+            key={item.id}
+            id={item.id}
+            title={item.boardTitle}
+            author={item.boardWriter}
+            content={item.boardContents}
+          />
+        );
+      });
+    setReviewList(reviews);
+  }, [reviewData]);
+
   return (
     <div className="accordion-body">
       <h1>나의 리뷰</h1>
       <div className="accordion">
         <hr />
-        <TableItem />
-        <TableItem />
-        <TableItem />
-        <TableItem />
-        <TableItem />
+        {reviewList}
       </div>
-      <button
-        id="myBtn"
-        onClick={() => {
-          const modal = document.querySelector('#myModal');
-          modal.classList.remove('hide');
-        }}
-      >
-        글쓰기
-      </button>
 
       <Modal action={'edit'} />
     </div>
