fb4ac0c4a22c3b95d05d0e5c4bcb523e5c406ebf
yujin <imyujinsim@gmail.com>
2023-04-03 10:22:45 +0900

    review test

---
+++ src/component/Modal.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+function Modal(props) {
+  return (
+    <div id="myModal" class="modal hide">
+      <div class="modal-content">
+        <span
+          class="close"
+          onClick={() => {
+            const modal = document.querySelector('#myModal');
+            modal.classList.add('hide');
+            console.log('clicked');
+          }}
+        >
+          &times;
+        </span>
+        <div className="form-data">
+          <h2>리뷰 작성</h2>
+          <form action="." method="post" className="reviewForm">
+            <div className="review-row">
+              <input
+                type="text"
+                name="boardWriter"
+                placeholder="작성자"
+                className="author"
+              />{' '}
+              <br />
+              <input
+                type="text"
+                name="boardPass"
+                placeholder="비밀번호"
+                className="password"
+              />{' '}
+              <br />
+            </div>
+            <input
+              type="text"
+              name="boardTitle"
+              placeholder="제목"
+              className="title"
+            />{' '}
+            <br />
+            <textarea
+              name="boardContents"
+              cols="30"
+              rows="5"
+              placeholder="내용 입력"
+              className="content"
+            ></textarea>
+            <br />
+            <button
+              className="submitBtn"
+              type="submit"
+              onSubmit={(event) => {
+                event.preventDefault();
+                console.log('submitted');
+                handleSubmit();
+                // const modal = document.querySelector('#myModal');
+                // modal.classList.add('hide');
+              }}
+            >
+              작성
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewTable;
--- src/component/ReviewTable.jsx
+++ src/component/ReviewTable.jsx
@@ -3,11 +3,12 @@ import axios from 'axios';
 import '../style/reset.css';
 import '../style/table.css';
 import TableItem from './TableItem';
+import Modal from './Modal';
 
 function ReviewTable(props) {
   const token = localStorage.getItem('token');
 
-  const onSubmit = async () => {
+  const handleSubmit = async () => {
     await axios({
       url: 'https://api.bodam.site:8080/board/save',
       method: 'post',
@@ -34,6 +35,7 @@ function ReviewTable(props) {
         console.log(error);
       });
   };
+
   return (
     <div className="accordion-body">
       <h1>리뷰</h1>
@@ -55,69 +57,7 @@ function ReviewTable(props) {
         글쓰기
       </button>
 
-      <div id="myModal" class="modal hide">
-        <div class="modal-content">
-          <span
-            class="close"
-            onClick={() => {
-              const modal = document.querySelector('#myModal');
-              modal.classList.add('hide');
-              console.log('clicked');
-            }}
-          >
-            &times;
-          </span>
-          <div className="form-data">
-            <h2>리뷰 작성</h2>
-            <form action="." method="post" className="reviewForm">
-              <div className="review-row">
-                <input
-                  type="text"
-                  name="boardWriter"
-                  placeholder="작성자"
-                  className="author"
-                />{' '}
-                <br />
-                <input
-                  type="text"
-                  name="boardPass"
-                  placeholder="비밀번호"
-                  className="password"
-                />{' '}
-                <br />
-              </div>
-              <input
-                type="text"
-                name="boardTitle"
-                placeholder="제목"
-                className="title"
-              />{' '}
-              <br />
-              <textarea
-                name="boardContents"
-                cols="30"
-                rows="5"
-                placeholder="내용 입력"
-                className="content"
-              ></textarea>
-              <br />
-              <button
-                className="submitBtn"
-                type="submit"
-                onClick={() => {
-                  // event.preventDefault();
-                  console.log('submitted');
-                  onSubmit();
-                  // const modal = document.querySelector('#myModal');
-                  // modal.classList.add('hide');
-                }}
-              >
-                작성
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Modal submit={handleSubmit} />
     </div>
   );
 }
