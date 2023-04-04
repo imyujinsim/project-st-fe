b0f7278d831ca6657fde1ba4b88bd90662a04970
yujin <imyujinsim@gmail.com>
2023-04-04 16:30:02 +0900

    review

--- src/component/TableItem.jsx
+++ src/component/TableItem.jsx
@@ -118,8 +118,7 @@ function TableItem(props) {
             <p
               className="remove"
               onClick={(e) => {
-                const id =
-                  e.target.parentNode.parentNode.parentNode.parentNode.id;
+                const id = e.target.parentNode.parentNode.parentNode.id;
                 console.log(id);
                 if (window.confirm('삭제하시겠습니까?')) {
                   // 삭제하는 코드
