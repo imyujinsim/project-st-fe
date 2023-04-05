ac1b0015db1f6e9f46dc2449d54d7d8e9d78750c
yujin <imyujinsim@gmail.com>
2023-04-05 10:15:43 +0900

    user info

--- src/mypage/MyInfo.jsx
+++ src/mypage/MyInfo.jsx
@@ -11,35 +11,34 @@ function MyInfo(props) {
   const [date, setDate] = useState();
   const token = localStorage.getItem('token');
 
-  useEffect(() => {
-    const getUserData = async () => {
-      await axios
-        .get(`https://api.bodam.site:8080/account/info`, {
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          console.log(response);
-          setUserData(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-    getUserData();
-  }, []);
+  const getUserData = async () => {
+    await axios
+      .get(`https://api.bodam.site:8080/account/info`, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  getUserData();
 
   useEffect(() => {
     console.log(userData);
-    setName(userData.name);
-    setId(userData.id);
-    setEmail(userData.email);
-    setTel(userData.tel);
-    setDate(userData.date);
+    // setName(userData.name);
+    // setId(userData.id);
+    // setEmail(userData.email);
+    // setTel(userData.tel);
+    // setDate(userData.date);
   }, [userData]);
 
   return (
@@ -51,31 +50,31 @@ function MyInfo(props) {
             <th>
               <b>이름</b>
             </th>
-            <td>{name}</td>
+            <td>{userData.name}</td>
           </tr>
           <tr>
             <th>
               <b>아이디</b>
             </th>
-            <td>{id}</td>
+            <td>{userData.id}</td>
           </tr>
           <tr>
             <th>
               <b>이메일</b>
             </th>
-            <td>{email}</td>
+            <td>{userData.email}</td>
           </tr>
           <tr>
             <th>
               <b>전화번호</b>
             </th>
-            <td>{tel}</td>
+            <td>{userData.tel}</td>
           </tr>
           <tr>
             <th>
               <b>생년월일</b>
             </th>
-            <td>{date}</td>
+            <td>{userData.date}</td>
           </tr>
         </tbody>
       </table>
