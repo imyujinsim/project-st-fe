2c01c08030bf26c2add5d19514b4725d028a0353
yujin <imyujinsim@gmail.com>
2023-04-05 10:20:12 +0900

    user info

--- src/mypage/MyInfo.jsx
+++ src/mypage/MyInfo.jsx
@@ -4,75 +4,71 @@ import '../style/myinfo.css';
 
 function MyInfo(props) {
   const [userData, setUserData] = useState();
-  const [name, setName] = useState();
-  const [id, setId] = useState();
-  const [email, setEmail] = useState();
-  const [tel, setTel] = useState();
-  const [date, setDate] = useState();
+
   const token = localStorage.getItem('token');
 
-  const getUserData = async () => {
-    await axios
-      .get(`https://api.bodam.site:8080/account/info`, {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        setUserData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  getUserData();
-
   useEffect(() => {
-    return (
-      <section className="user-information">
-        <h1>내 정보</h1>
-        <table className="user-info">
-          <tbody>
-            <tr>
-              <th>
-                <b>이름</b>
-              </th>
-              <td>{userData.name}</td>
-            </tr>
-            <tr>
-              <th>
-                <b>아이디</b>
-              </th>
-              <td>{userData.id}</td>
-            </tr>
-            <tr>
-              <th>
-                <b>이메일</b>
-              </th>
-              <td>{userData.email}</td>
-            </tr>
-            <tr>
-              <th>
-                <b>전화번호</b>
-              </th>
-              <td>{userData.tel}</td>
-            </tr>
-            <tr>
-              <th>
-                <b>생년월일</b>
-              </th>
-              <td>{userData.date}</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    );
-  }, [userData]);
+    const getUserData = async () => {
+      await axios
+        .get(`https://api.bodam.site:8080/account/info`, {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setUserData(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    getUserData();
+  }, [token]);
+
+  return (
+    <section className="user-information">
+      <h1>내 정보</h1>
+      <table className="user-info">
+        <tbody>
+          <tr>
+            <th>
+              <b>이름</b>
+            </th>
+            <td>{userData && userData.name}</td>
+          </tr>
+          <tr>
+            <th>
+              <b>아이디</b>
+            </th>
+            <td>{userData && userData.id}</td>
+          </tr>
+          <tr>
+            <th>
+              <b>이메일</b>
+            </th>
+            <td>{userData && userData.email}</td>
+          </tr>
+          <tr>
+            <th>
+              <b>전화번호</b>
+            </th>
+            <td>{userData && userData.tel}</td>
+          </tr>
+          <tr>
+            <th>
+              <b>생년월일</b>
+            </th>
+            <td>{userData && userData.date}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
 }
 
 export default MyInfo;
